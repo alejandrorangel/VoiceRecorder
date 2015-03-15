8ccbeca5b94b0a268b3f2e2c1190a15e96c67101
Alejandro <arangel@cicese.edu.mx>
2015-03-15 11:55:51 -0700

    * Change from Stereo to Mono

--- app/src/main/java/mx/edu/cicese/alejandro/voicerecorder/MainActivity.java
+++ app/src/main/java/mx/edu/cicese/alejandro/voicerecorder/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends Activity {
     private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
     private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
     private static final int RECORDER_SAMPLERATE = 44100;
-    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
     private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
 
     private AudioRecord recorder = null;
@@ -250,7 +250,7 @@ public class MainActivity extends Activity {
         long totalAudioLen = 0;
         long totalDataLen = totalAudioLen + 36;
         long longSampleRate = RECORDER_SAMPLERATE;
-        int channels = 2;
+        int channels = 1;
         long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels / 8;
 
         byte[] data = new byte[bufferSize];
